42d812676cae8748f082973dd551b69c203ebc80
Will <will@containerlabs.io>
2023-04-19 02:12:46 -0700

    init scaleway redis

---
+++ src/_artifacts.tf
@@ -0,0 +1,38 @@
+locals {
+  data_infrastructure = {
+    id = scaleway_redis_cluster.main.id
+  }
+
+  data_authentication = {
+    username = scaleway_redis_cluster.main.user_name
+    password = scaleway_redis_cluster.main.password
+    # one host per node when size > 1
+    # hostname = scaleway_redis_cluster.main.host
+    # port     = scaleway_redis_cluster.main.port
+  }
+
+  data_security = {
+    iam = {
+      "read/write" = {
+        permission_set = "RedisReadOnly"
+      }
+    }
+  }
+
+  artifact = {
+    data = {
+      infrastructure = local.data_infrastructure
+      authentication = local.data_authentication
+      security       = local.data_security
+    }
+    specs = {
+    }
+  }
+}
+
+resource "massdriver_artifact" "authentication" {
+  field                = "authentication"
+  provider_resource_id = scaleway_redis_cluster.main.id
+  name                 = ""
+  artifact             = jsonencode(local.artifact)
+}
