4e167133d94157b64be94113d4575937fe397b4e
Will <will@containerlabs.io>
2023-04-20 04:42:44 -0700

    fmt

--- src/main.tf
+++ src/main.tf
@@ -1,6 +1,6 @@
 module "redis_cluster" {
   source             = "container-labs/redis/scaleway"
-  version = "~> 0.2"
+  version            = "~> 0.2"
   name               = var.md_metadata.name_prefix
   tags               = var.md_metadata.default_tags
   private_network_id = var.network.data.infrastructure.id
