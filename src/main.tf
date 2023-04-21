ddbb4dceb0a25d85e2a290e168d6ccb218726b42
Will <will@containerlabs.io>
2023-04-21 09:50:42 -0700

    url update

--- src/main.tf
+++ src/main.tf
@@ -4,7 +4,7 @@ module "redis_cluster" {
   name               = var.md_metadata.name_prefix
   tags               = var.md_metadata.default_tags
   private_network_id = var.network.data.infrastructure.id
-  zone               = var.network.specs.scw.zones[0]
+  zone               = var.network.specs.scaleway.zones[0]
   node_type          = var.node_type
   cluster_mode       = var.cluster_mode
 }
